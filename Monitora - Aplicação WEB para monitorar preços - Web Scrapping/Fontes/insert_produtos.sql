5b557be545f5f9031a9e43f3241ecd74ab9469ea
Edilson Hipolito da Silva <edilsonhipolito@gmail.com>
2019-11-28 07:05:51 -0200

    Adicionado projeto Monitora

---
+++ Monitora - Aplicação WEB para monitorar preços - Web Scrapping/Fontes/insert_produtos.sql
@@ -0,0 +1,10 @@
+INSERT INTO db_monitora.produtos_produto (id_produto, nome, descricao, preco_atual, data_cadastro, url, unique_id, tempo_notificacao)
+VALUES 
+	(1, 'teste1', '------------------', 'R$ 50', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (2, 'teste2', '------------------', 'R$ 5', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (3, 'teste3', '------------------', 'R$ 10', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (4, 'teste4', '------------------', 'R$ 9', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (5, 'teste5', '------------------', 'R$ 99,99', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (6, 'teste6', '------------------', 'R$ 1000,50', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (7, 'teste7', '------------------', 'R$ 505', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10'),
+    (8, 'teste8', '------------------', 'R$ 101.3', '2008-11-11 13:23:44', 'https://www.youtube.com/watch?v=zh9QvsahGMQ', 'sidufbhsd87fsdj', '10');
---
+++ Monitora - Aplicação WEB para monitorar preços - Web Scrapping/Fontes/insert_produtos_user.sql
@@ -0,0 +1,11 @@
+SELECT * FROM db_monitora.produtos_produto_user;
+INSERT INTO db_monitora.produtos_produto_user (id, produto_id, user_id)
+VALUES 
+	(1, 1, 19),
+    (2, 2, 19),
+    (3, 3, 19),
+    (4, 4, 19),
+    (5, 5, 19),
+    (6, 6, 19),
+    (7, 7, 19),
+    (8, 8, 19);
